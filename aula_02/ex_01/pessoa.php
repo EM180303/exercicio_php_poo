53cdb11cd065f938a5af788dc3cd3f940698ce93
Edu Marques <e.marques180303@gmail.com>
2021-02-25 00:38:51 -0300

    get e set

--- aula_02/ex_01/pessoa.php
+++ aula_02/ex_01/pessoa.php
@@ -3,7 +3,39 @@ class Pessoa{
     public $nome;
     public $cpf;
     public $rg;
-    public $datanasc;
+    public $data_Nasc;
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setNome($nome){
+        return $this->nome = $nome;
+    }
+
+    public function getCpf(){
+        return $this->cpf;
+    }
+
+    public function setCpf($cpf){
+        return $this->cpf = $cpf;
+    }
+
+    public function getRg(){
+        return $this->rg;
+    }
+
+    public function setRg($rg){
+        return $this->rg = $rg;
+    }
+
+    public function getData_Nasc(){
+        return $this->data_Nasc;
+    }
+
+    public function setData_Nasc($data_Nasc){
+        return $this->data_Nasc = $data_Nasc;
+    }
  
 }
 
